c0323cce560f989d088f66bf6e1937d81bbbbd67
Dennis Stoklosa <dennis.stoklosa@sparqs.io>
2020-01-28 20:59:06 +0100

    implemented angular router to navigate through components

--- desto/src/app/app-routing.module.ts
+++ desto/src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { DashboardComponent } from './core/dashboard/dashboard.component';
 import { MycareerComponent } from './modules/mycareer/mycareer.component';
 import { PrivacypolicyComponent } from './core/privacypolicy/privacypolicy.component';
 import { ContactmeComponent } from './modules/contactme/contactme.component';
+import { MyprojectsComponent } from './modules/myprojects/myprojects.component';
+import { MyskillsComponent } from './modules/myskills/myskills.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -12,6 +14,9 @@ const routes: Routes = [
   { path: 'legals', component: LegalsComponent},
   { path: 'privacy', component: PrivacypolicyComponent},
   { path: 'contact', component: ContactmeComponent},
+  { path: 'projects', component: MyprojectsComponent},
+  { path: 'skills', component: MyskillsComponent},
+  { path: 'career', component: MycareerComponent},
   { path: 'examples', component: MycareerComponent} // TO DO remove, just tests
 ];
 
