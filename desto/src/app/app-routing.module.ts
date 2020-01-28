bdf6826aa93614ed5062b554213ced818e9f0d5c
Dennis Stoklosa <dennis.stoklosa@sparqs.io>
2020-01-28 20:31:52 +0100

    adjusted navbar

--- desto/src/app/app-routing.module.ts
+++ desto/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LegalsComponent } from './core/legals/legals.component';
 import { DashboardComponent } from './core/dashboard/dashboard.component';
 import { MycareerComponent } from './modules/mycareer/mycareer.component';
 import { PrivacypolicyComponent } from './core/privacypolicy/privacypolicy.component';
+import { ContactmeComponent } from './modules/contactme/contactme.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent},
   { path: 'legals', component: LegalsComponent},
   { path: 'privacy', component: PrivacypolicyComponent},
+  { path: 'contact', component: ContactmeComponent},
   { path: 'examples', component: MycareerComponent} // TO DO remove, just tests
 ];
 
