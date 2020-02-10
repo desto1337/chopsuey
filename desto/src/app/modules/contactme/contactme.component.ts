67ddfc719b7138eb9d536c78f5833786e0c74a55
Dennis Stoklosa <dennis.stoklosa@sparqs.io>
2020-02-10 15:31:25 +0100

    contact form improvements

--- desto/src/app/modules/contactme/contactme.component.ts
+++ desto/src/app/modules/contactme/contactme.component.ts
@@ -7,12 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactmeComponent implements OnInit {
 
-  private disabledGenerateEmail = true;
+  private mailtoString = 'contact@dennis-stoklosa.de';
   private contactname: string;
-  private subject: string;
+  private subject = 'Feedback';
   private message: string;
+  private messageHtml: any;
 
     constructor() { }
 
-    ngOnInit() {}
+    ngOnInit() {
+      if (this.contactname || this.subject || this.message) {
+
+        this.mailtoString = 'contact@dennis-stoklosa.de?';
+        this.mailtoString = 'contact@dennis-stoklosa.de?subject=' + this.subject; // subject ist immer ausgewählt
+
+        if (this.message) {
+          this.mailtoString = this.mailtoString + '&body=' + this.message;
+        }
+
+      } else {
+        this.mailtoString = 'contact@dennis-stoklosa.de';
+      }
+    }
+
+    updateMailtoString() {
+
+      if (this.contactname || this.subject || this.message) {
+
+        this.mailtoString = 'contact@dennis-stoklosa.de?';
+        this.mailtoString = 'contact@dennis-stoklosa.de?subject=' + this.subject; // subject ist immer ausgewählt
+
+        if (this.message) {
+          this.mailtoString = this.mailtoString + '&body=' + this.message;
+        }
+
+      } else {
+        this.mailtoString = 'contact@dennis-stoklosa.de';
+      }
+    }
   }
