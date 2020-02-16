e136310754a7b19303c3413d20260a23fa30088a
Dennis Stoklosa <dennis.stoklosa@sparqs.io>
2020-02-16 13:07:33 +0100

    initialized apexchart example with own theme colorway

--- desto/src/app/modules/myskills/myskills.component.ts
+++ desto/src/app/modules/myskills/myskills.component.ts
@@ -3,6 +3,7 @@ import { ContentfulService } from 'src/app/core/services/contentful/contentful.s
 import { SkillFields } from 'src/app/models/skill/skillFields';
 import { Entry } from 'contentful';
 import { TechnologyLayerDescription } from 'src/app/models/technologyLayerDescription/technologyLayerDescription';
+import { ApexChart, ApexTitleSubtitle, ApexAxisChartSeries, ApexXAxis, ApexTheme } from 'ng-apexcharts';
 
 @Component({
   selector: 'app-myskills',
@@ -13,6 +14,14 @@ export class MyskillsComponent implements OnInit {
 
   private skills: SkillFields[];
   private technologyDescriptions: TechnologyLayerDescription[];
+
+  // chart stuff
+  private chartType: ApexChart;
+  private chartTheme: ApexTheme;
+  private frontendChartTitle: ApexTitleSubtitle;
+
+  private frontendChartSeries: ApexAxisChartSeries;
+  private frontendChartXaxis: ApexXAxis;
 
   constructor(private contentfulService: ContentfulService) { }
 
@@ -30,6 +39,10 @@ export class MyskillsComponent implements OnInit {
     }).catch(error => {
       console.log('Contentful-API: Es wurden keine Technology-Beschreibungen gefunden: ', error);
     });
+
+
+    /* */
+    this.initializeCharts();
   }
 
   resolveInnerEntriesOfType<T>(entries: Entry<T>[]): T[] {
@@ -40,5 +53,48 @@ export class MyskillsComponent implements OnInit {
     });
 
     return innerEntryTypes;
+  }
+
+  initializeCharts() {
+    this.generateChartsBasicTheme();
+    this.setChartTitles();
+    this.setFrontendChartSeries();
+    this.setFrontendXaxis();
+  }
+
+  generateChartsBasicTheme() {
+    const chart: ApexChart = {
+      type: 'bar'
+    };
+
+    this.chartType = chart;
+
+    this.chartTheme = {
+      monochrome: {
+          enabled: true,
+          color: '#6597e7',
+          shadeTo: 'light',
+          shadeIntensity: 0.65
+        }
+    };
+  }
+
+  setChartTitles() {
+    this.frontendChartTitle = {
+      text: 'Frontend-Titel'
+    };
+  }
+
+  setFrontendChartSeries() {
+    this.frontendChartSeries = [{
+      name: 'sales',
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+    }];
+  }
+
+  setFrontendXaxis() {
+    this.frontendChartXaxis = {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+    };
   }
 }
